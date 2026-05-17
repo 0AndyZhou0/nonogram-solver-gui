5e6b7d939c7b5aaa521bc827dfeaa2a959c24966
Andy Zhou <zhoua2001@gmail.com>
2026-05-17 16:48:21 -0400

    fix row and col validation

--- nonogram-board.hpp
+++ nonogram-board.hpp
@@ -16,131 +16,139 @@ namespace NonogramSolver {
 // };
 
 class NonogramBoard {
-  public:
+public:
       NonogramBoard(size_t w, size_t h) {
         width = w;
         height = h;
         rows_restrictions = std::vector<std::vector<u_int32_t>>(h);
         cols_restrictions = std::vector<std::vector<u_int32_t>>(w);
         board = std::vector<std::vector<int32_t>>(h, std::vector<int32_t>(w, 0));
-      }
+    }
 
-      int get_width() {
+    size_t get_width() {
         return width;
-      }
+    }
 
-      int get_height() {
+    size_t get_height() {
         return height;
-      }
+    }
 
-      std::vector<u_int32_t> get_row_restriction(size_t r) {
+    std::vector<u_int32_t> get_row_restriction(size_t r) {
         return rows_restrictions[r];
-      }
+    }
 
-      std::vector<u_int32_t> get_col_restriction(size_t c) {
+    std::vector<u_int32_t> get_col_restriction(size_t c) {
         return cols_restrictions[c];
-      }
+    }
 
-      void set_row_restriction(size_t r, std::vector<u_int32_t> row_restrictions) {
+    void set_row_restriction(size_t r, std::vector<u_int32_t> row_restrictions) {
         assert(std::reduce(row_restrictions.begin(), row_restrictions.end()) + row_restrictions.size() - 1 <= width);
         std::vector<u_int32_t> copy(row_restrictions);
         rows_restrictions[r] = copy;
-      }
+    }
 
-      void set_col_restriction(size_t c, std::vector<u_int32_t> column_restrictions) {
+    void set_col_restriction(size_t c, std::vector<u_int32_t> column_restrictions) {
         assert(std::reduce(column_restrictions.begin(), column_restrictions.end()) + column_restrictions.size() - 1 <= height);
         std::vector<u_int32_t> copy(column_restrictions);
         cols_restrictions[c] = copy;
-      }
+    }
 
-      std::vector<int32_t> get_row(size_t r) {
+    std::vector<int32_t> get_row(size_t r) {
         return std::vector<int32_t>(board[r]);
-      }
+    }
 
-      std::vector<int32_t> get_col(size_t c) {
+    std::vector<int32_t> get_col(size_t c) {
         std::vector<int32_t> res(height);
         for (size_t i = 0; i < height; i++) {
-          res[i] = board[i][c];
+            res[i] = board[i][c];
         }
         return res;
-      }
+    }
 
-      int get(size_t r, size_t c) {
+    int get(size_t r, size_t c) {
         return board[r][c];
-      }
+    }
 
-      void mark_impossible(size_t r, size_t c) {
+    void mark_impossible(size_t r, size_t c) {
         board[r][c] = -1;
-      }
+    }
 
-      void mark_sudoimpossible(size_t r, size_t c) {
+    void mark_sudoimpossible(size_t r, size_t c) {
         board[r][c] = -2;
-      }
+    }
 
-      void mark_ambiguous(size_t r, size_t c) {
+    void mark_ambiguous(size_t r, size_t c) {
         board[r][c] = -3;
-      }
+    }
 
-      void mark_possible(size_t r, size_t c) {
+    void mark_possible(size_t r, size_t c) {
         board[r][c] = 2;
-      }
+    }
 
-      void set(size_t r, size_t c) {
+    void set(size_t r, size_t c) {
         board[r][c] = 1;
-      }
+    }
 
-      void unset(size_t r, size_t c) {
+    void unset(size_t r, size_t c) {
         board[r][c] = 0;
-      }
+    }
 
-      bool validate_row(size_t r) {
+    bool validate_row(size_t r) {
         std::vector<u_int32_t> restrictions = rows_restrictions[r];
         u_int32_t count = 0;
         size_t curr = 0;
         for (size_t c = 0; c < width; c++) {
-          if (board[r][c] == 1) {
-            count += 1;
-          } else {
-            if (count > 0) {
-              if (curr >= restrictions.size() || restrictions[curr] != count) {
-                return false;
-              }
-              curr += 1;
+            if (board[r][c] == 1) {
+                count += 1;
+            } else {
+                if (count > 0) {
+                    if (curr >= restrictions.size() || restrictions[curr] != count) {
+                        return false;
+                    }
+                    curr += 1;
+                }
+                count = 0;
             }
-            count = 0;
-          }
         }
-        if (curr >= restrictions.size() || restrictions[curr] != count) {
-          return false;
+        if (count > 0) {
+            if (curr != restrictions.size()-1 || restrictions[curr] != count) {
+                return false;
+            }
+        } else if (curr != restrictions.size()) {
+            return false;
         }
         return true;
-      }
+    }
 
-      bool validate_column(size_t c) {
+    bool validate_column(size_t c) {
         std::vector<u_int32_t> restrictions = cols_restrictions[c];
         u_int32_t count = 0;
         size_t curr = 0;
         for (size_t r = 0; r < height; r++) {
-          if (board[r][c] == 1) {
-            count += 1;
-          } else {
-            if (count > 0) {
-              if (curr >= restrictions.size() || restrictions[curr] != count) {
-                return false;
-              }
-              curr += 1;
+            if (board[r][c] == 1) {
+                count += 1;
+            } else {
+                if (count > 0) {
+                    if (curr >= restrictions.size() || restrictions[curr] != count) {
+                        return false;
+                    }
+                    curr += 1;
+                }
+                count = 0;
             }
-            count = 0;
-          }
         }
-        if (curr >= restrictions.size() || restrictions[curr] != count) {
-          return false;
+        if (count > 0) {
+            if (curr != restrictions.size()-1 || restrictions[curr] != count) {
+                return false;
+            }
+        } else if (curr != restrictions.size()) {
+            return false;
         }
         return true;
-      }
-      
+    }
 
-  private:
+
+private:
     size_t width;
     size_t height;
     std::vector<std::vector<u_int32_t>> rows_restrictions;
